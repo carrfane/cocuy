16b4092209a24b920f823ae041a1ede7d40a26a4
Javier <lestat998@gmail.com>
2017-06-24 22:31:47 -0400

    portafolio

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-  $('#pagepilling').pagepiling({
+
+var jQuery_3_1_1 = $.noConflict(true);
+
+jQuery_3_1_1(document).ready(function(){
+  jQuery_3_1_1('#pagepilling').pagepiling({
     direction: 'horizontal',
     loopBottom: true,
     anchors: ['home', 'nosotros','portfolio']
